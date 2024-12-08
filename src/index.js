cea0b8827c92b160824238ddbb2901e1b0b97b78
Shrey <guptashrey163@gmail.com>
2024-12-08 15:02:37 +0530

    Feat:Added Prettier and eslint to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import express from "express";
-import statuscodes from "http-status-codes";
-import { PORT } from "./Config/serverConfig.js";
+import express from 'express';
+import statuscodes from 'http-status-codes';
+import { PORT } from './Config/serverConfig.js';
 
+const app = express();
 
-const app=express();
+app.get('/ping', (req, res) => {
+  return res.status(statuscodes.OK).json({ message: 'pong' });
+});
 
-app.get("/ping",(req,res)=>{
-    return res.status(statuscodes.OK).json({message:"pong"});
-})
-
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
