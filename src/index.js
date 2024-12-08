327f1509cca0b1b7592ec41cc0f6aabcc90b2d1b
Shrey <guptashrey163@gmail.com>
2024-12-08 13:44:51 +0530

    Feat:Slack backend server setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import statuscodes from "http-status-codes";
+import { PORT } from "./Config/serverConfig.js";
+
+
+const app=express();
+
+app.get("/ping",(req,res)=>{
+    return res.status(statuscodes.OK).json({message:"pong"});
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+})
