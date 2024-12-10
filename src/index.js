e0146ee7fdcc272a2a73410b2802a15b866db486
Shrey <guptashrey163@gmail.com>
2024-12-10 16:50:40 +0530

    Feat:Router Setup for /api/v1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import statuscodes from 'http-status-codes';
 // import connectToDb from './Config/dbConfig.js';
 import connectDb from './Config/dbConfig.js';
 import { PORT } from './Config/serverConfig.js';
+import apiRoutes from "./Routes/apiRoutes.js"
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/ping', (req, res) => {
   return res.status(statuscodes.OK).json({ message: 'pong' });
 });
+app.use("/api",apiRoutes)
 
 app.listen(PORT, () => {
    
